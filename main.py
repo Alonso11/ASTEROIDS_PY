44866f9873fb96251c09846fbc70c4fcf11ca5f6
alonso11 <fabi.goque@hotmail.com>
2025-07-04 16:37:50 -0600

    visual object to be used
    drawing the player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import constants 
 import pygame
+import player
 
 def main():
     pygame.init()
@@ -10,13 +11,19 @@ def main():
     print("Screen height:",constants.SCREEN_HEIGHT)
    
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+
+    player1 = player.Player(x,y)
 
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         clock.tick(60)
     dt =clock/1000
