91d456c236e9305b44dc1694bb387daa83cebe6a
alonso11 <fabi.goque@hotmail.com>
2025-07-04 14:01:27 -0600

    initialize modules

---
+++ main.py
@@ -0,0 +1,8 @@
+import constants 
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:",constants.SCREEN_WIDTH)
+    print("Screen height:",constants.SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
